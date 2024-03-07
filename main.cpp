3e1e2ac948b90b8d5aa4d4fe0b2ad5cc64a6813f
alenchen <a0961101939@gmail.com>
2024-03-07 19:30:51 +0800

    create branch test

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@ using namespace std;
 
 //struct node
 typedef struct node{
-    //node *next;
     int vertex=-1;
     int sup=-1;
-    int lowerBound_k=-1;
-    int upperBound_k=-1;
+    int k=-1;
+    //int lowerBound_k=-1;
+    //int upperBound_k=-1;
 
 }NODE;
 
-
+struct EDGE{
+    int s;
+    int t;
+};
 //class graph
 class Graph{
     public:
@@ -53,7 +56,7 @@ class Graph{
             if (outfile.is_open()) {
                 for (int i=0;i< adj.size();i++) {
                     for(auto it = this->adj[i].begin(); it != this->adj[i].end(); it++)
-                        outfile << i<<" "<< it->vertex<< "\n";
+                        outfile << i<<" "<< it->vertex<< " "<<it->sup<<"\n";
                 }
                 outfile.close();
             } else {
@@ -71,18 +74,27 @@ class Graph{
             int sup=0;
             Q1=tau_hop_neighbor(v);
             Q2=tau_hop_neighbor(w);
-            
+            //find MNN nodes
             for(int i=0; i<Q1.size(); i++){
                 for(int j=0; j<Q2.size(); j++)
                     if(Q1[i]==Q2[j])
                         sup++;     
             }
+            
+            // cout<<"\n v neighbors:";
+            // for(int i=0; i<Q1.size(); i++)
+            //     cout<<Q1[i]<<" ";
+            // cout<<"\n u neighbors:";
+            // for(int i=0; i<Q2.size(); i++)
+            //     cout<<Q2[i]<<" ";
+            
+            // cout<<"\n sup = "<<sup<<endl;
 
-            cout<<"sup: "<<sup<<endl;
+            // e(u,v) sup
             for(auto it = adj[v].begin(); it != adj[v].end(); it++)
                 if(it->vertex==w)
                     it->sup=sup;
-
+            // e(v,u) sup
             for(auto it = adj[w].begin(); it != adj[w].end(); it++)
                 if(it->vertex==v)
                     it->sup=sup;
@@ -90,8 +102,56 @@ class Graph{
             return;
         }
 
-        
+        void compute_ALL_support(int *small_sup){
+            for (int i=0;i< adj.size();i++) {
+                    for(auto it = adj[i].begin(); it != adj[i].end(); it++)
+                        if(i< it->vertex){
+                            compute_edge_support(i,it->vertex);
+                            if(it->sup<*small_sup)
+                                *small_sup=it->sup;
+                        }
+                            
+                }
+        }
 
+
+        vector<EDGE> effect_edge(int u,int v){
+            vector<int> Q1, Q2;
+            vector<EDGE> edge;
+            Q1=tau_hop_neighbor(u);
+            Q2=tau_hop_neighbor(v);
+            
+            cout<<"\n u neighbors:";
+            for(int i=0; i<Q1.size(); i++)
+                cout<<Q1[i]<<" ";
+            cout<<"\n v neighbors:";
+            for(int i=0; i<Q2.size(); i++)
+                cout<<Q2[i]<<" ";
+
+            cout<<endl;
+            vector<int> MNN;
+            MNN.push_back(u);
+            MNN.push_back(v);
+            for(int i=0; i<Q1.size(); i++){
+                for(int j=0; j<Q2.size(); j++)
+                    if(Q1[i]==Q2[j])
+                        MNN.push_back(Q1[i]);   
+            }
+
+
+            for(int i=0; i<MNN.size(); i++)
+                for(int j=0; j<MNN.size(); j++)
+                    if((MNN[i]<MNN[j])&&find_edge(MNN[i],MNN[j]))
+                        edge.push_back({MNN[i],MNN[j]});
+                
+            
+
+            for(auto it=edge.begin(); it!=edge.end(); it++)
+                if((it->s==u&&it->t==v)||(it->s==v&&it->t==u))
+                    it=edge.erase(it);
+   
+            return edge;
+        }
     private:
         vector<int> tau_hop_neighbor(int v){
             queue<int> q;
@@ -123,6 +183,12 @@ class Graph{
             return Q1;
         }
         
+        bool find_edge(int u,int v){
+            for(auto it = adj[u].begin(); it != adj[u].end(); it++)
+                if(it->vertex==v)
+                    return true;
+            return false;
+        }
 };
 
 bool check_any_edge(Graph G){
@@ -171,6 +237,20 @@ bool read_file(string filename ,int *nodes,int *edges,Graph *G_in,int tau){
     return true;
 }
 
+
+
+bool edge_smaller_than_k(vector<list<node>> graph_adj,int k,int *u,int *v){
+    for (int i=0;i< graph_adj.size();i++) {
+        for(auto it = graph_adj[i].begin(); it != graph_adj[i].end(); it++)
+            if(it->sup<=k){
+                *u=i;
+                *v=it->vertex;
+                return true;
+            }                    
+    }
+    return false;
+}
+
 int main(){
     Graph G_input;
     int node_num =0, edge_num =0;
@@ -179,17 +259,46 @@ int main(){
     string filename="./dataset/test.txt";//graph
     int tau=2;
 
-
+    //read file
     if(!read_file(filename,&node_num,&edge_num,&G_input,tau))
         return EXIT_FAILURE;
     
 
-    cout << "Nodes: " << node_num << endl;
-    cout << "Edges: " << edge_num << endl;
+    // cout << "Nodes: " << node_num << endl;
+    // cout << "Edges: " << edge_num << endl;
 
-    G_input.compute_edge_support(2,1);
-    // G_input.printGraph();
-    //G_input.python_draw_graph();
+    //compute_ALL_support
+    int min_k=INT32_MAX;
+    G_input.compute_ALL_support(&min_k);
+
+    Graph graph_adj=G_input;
+    
+    int u,v;
+    while(edge_smaller_than_k(graph_adj.adj,min_k,&u,&v)){
+        vector<EDGE> effect_edge;
+        //put k_truss in edge(u,v)
+        for(auto it = G_input.adj[u].begin(); it != G_input.adj[u].end(); it++)
+            if(it->vertex=v)
+                it->k=min_k+2;
+        for(auto it = G_input.adj[v].begin(); it != G_input.adj[v].end(); it++)
+            if(it->vertex=u)
+                it->k=min_k+2;
+
+        //G'<-G\e(u,v)
+        Graph graph_before=graph_adj;
+        graph_adj.removeEdge(u,v);
+        cout<<"remove edge: ("<<u<<" , "<<v<<")"<<endl;
+        //effect edge(u,v) after delete u,v
+        effect_edge=graph_before.effect_edge(u,v);
+        
+        cout<<"\n effect_edge size: "<<effect_edge.size()<<endl;
+        for(int i=0;i<effect_edge.size();i++){
+            cout<<effect_edge[i].s<<" "<<effect_edge[i].t<<endl;
+        }
+    }
+
+    //graph_adj.python_draw_graph();
+    
     
     //start HOtdecom
     // while(check_any_edge(G_input)){
--- show_graph.py
+++ show_graph.py
@@ -10,18 +10,20 @@ with open(file_name, 'r') as file:
 # 解析邊的資訊
 u = []
 v = []
+sup = []
 for line in lines:
     edge = line.split()
     u.append(int(edge[0]))
     v.append(int(edge[1]))
+    sup.append(int(edge[2]))
 
 # 建立圖形
 G = nx.Graph()
 for i in range(len(u)):
-    G.add_edge(u[i], v[i])
+    G.add_edge(u[i], v[i], weight=sup[i])  # 在這裡將weight加入邊的屬性
 
 # 繪製圖形
-nx.draw(G, with_labels=True)
+pos = nx.spring_layout(G, k=1)  # 設置 k 值為較大的值
+nx.draw(G, pos, with_labels=True, node_size=70, node_color='blue', font_size=10)  # 繪製節點
+nx.draw_networkx_edge_labels(G, pos, edge_labels={(u[i], v[i]): sup[i] for i in range(len(u))})  # 顯示邊的權重
 plt.show()
-
-plt.close()
